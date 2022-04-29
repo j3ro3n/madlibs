9c66e9a34e02af2e13c461308bf058c1a6d18b25
Jeroen Buchholtz <wh.buchholtz@student.avans.nl>
2022-04-29 15:36:24 +0200

    Initial commit with Angular Framework and demo index.html text

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'ml-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'Madlibs';
 }
+
