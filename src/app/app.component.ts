02e77f565a089f51bbd4e427048c8de24a035d55
AceTMWC <56593048+AceTMWC@users.noreply.github.com>
2022-06-11 08:54:51 +0200

    Re-init to fix outdated dependencies

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'ml-app',
+  selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Madlibs';
+  title = 'MadderLibs';
 }
-
